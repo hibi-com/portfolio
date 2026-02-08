c87636ea5061603d9cf81cef0a8feb88f7ce4976
ageha734 <hibi-keita@dmm.com>
2026-02-09 04:15:30 +0900

    chore: remove obsolete Astro content and configuration files to streamline project structure and enhance maintainability

--- apps/wiki/.astro/content-modules.mjs
+++
@@ -1,5 +0,0 @@
-
-export default new Map([
-["src/content/docs/reports/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freports%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/reports/coverage.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Freports%2Fcoverage.mdx&astroContentModuleFlag=true")]]);
-		
