26648ae00e151676be09ff5087340c6ca5bc5c5a
ageha734 <hibi-keita@dmm.com>
2026-02-10 16:38:03 +0900

    refactor: reorganize exports in index.ts to enhance structure and accessibility, adding missing components while removing duplicates for improved clarity

--- packages/log/src/index.ts
+++ packages/log/src/index.ts
@@ -1,32 +1,29 @@
 export { AppError } from "./errors/app-error";
 export {
     ErrorCategory,
-    ErrorCodes,
     type ErrorCode,
+    ErrorCodes,
     getErrorCategory,
     getHttpStatusFromErrorCode,
 } from "./errors/error-codes";
-
-export { initSentry, closeSentry, type SentryConfig } from "./sentry/config";
-export { SentryClient, sentryClient } from "./sentry/client";
-
+export {
+    defaultFormatter,
+    JsonFormatter,
+    type LogEntry,
+    type LogFormatter,
+    PlainTextFormatter,
+} from "./logger/formatter";
+export { compareLogLevel, LogLevel, LogLevelPriority } from "./logger/levels";
+export {
+    defaultLogger,
+    Logger,
+    type LoggerConfig,
+} from "./logger/logger";
+export { PrometheusClient } from "./prometheus/client";
 export {
     createPrometheusRegistry,
     type PrometheusConfig,
 } from "./prometheus/config";
-export { PrometheusClient } from "./prometheus/client";
 export { CommonMetrics } from "./prometheus/metrics";
-
-export { LogLevel, LogLevelPriority, compareLogLevel } from "./logger/levels";
-export {
-    JsonFormatter,
-    PlainTextFormatter,
-    defaultFormatter,
-    type LogEntry,
-    type LogFormatter,
-} from "./logger/formatter";
-export {
-    Logger,
-    defaultLogger,
-    type LoggerConfig,
-} from "./logger/logger";
+export { SentryClient, sentryClient } from "./sentry/client";
+export { closeSentry, initSentry, type SentryConfig } from "./sentry/config";
